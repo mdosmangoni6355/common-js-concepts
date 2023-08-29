79701de36307edf121035bdad45d5b8617f88383
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-08-30 04:53:06 +0600

    hoisting.js file added

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+// print10();
+console.log(output);
+// print20();
+for(var i = 0; i < 5; i++){
+    // console.log(i);
+}
+// console.log('Outside of', i);
+
+function print10(){
+    console.log("Inside print10", 10);
+}
+
+var output = function(){
+    console.log("inside print20", 20);
+}
