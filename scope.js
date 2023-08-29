4504d025b6e9bd35a407d1fcab0b98870cea6498
mdosmangoni6355 <mdosmangoni6355@gmail.com>
2023-08-30 03:42:06 +0600

    scope.js file added

---
+++ scope.js
@@ -0,0 +1,16 @@
+function number(a, b){
+    const total = a + b;
+    if(b>5){
+        const sum = 25 + a + b;
+    }
+    else{
+        const sum = 5 + a + b;
+        var current = sum;
+    }
+    console.log(current);
+    // console.log(sum);
+    return total;
+}
+// console.log(a, b);
+// console.log(total);
+number(5, 3);
